f26d36ac9bdee5ebbf0b4f4add7a483dd77341e6
Victor Gorokhov <me@rkit.pp.ru>
2014-11-03 15:25:45 +0300

    Changed angularjs version to 1.2.26

--- public/php/comments.php
+++ public/php/comments.php
@@ -5,8 +5,8 @@ $root = preg_replace('/php\/comments.php$/', '', $_SERVER['REQUEST_URI']);
 commentsRoot = '<?php echo $root;?>';
 
 document.write(
-'<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.6/angular.min.js"></script>\
-<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.0.6/angular-cookies.min.js"></script>\
+'<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.min.js"></script>\
+<script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-cookies.min.js"></script>\
 <script src="'+ commentsRoot +'./static/model.js"></script>\
 <div xmlns:ng="http://angularjs.org" id="ng-app" ng-app="Comments">\
     <div ng-include src="\''+ commentsRoot +'./static/comments.htm\'"></div>\
