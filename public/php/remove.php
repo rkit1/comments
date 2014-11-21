a82bf781a8ff4211771c06f8acb3050e77078c45
Victor Gorokhov <me@rkit.pp.ru>
2014-11-22 02:04:46 +0300

    Delete fix 2

--- public/php/remove.php
+++ public/php/remove.php
@@ -7,7 +7,7 @@ JSON::Setup();
 $s = Session::CheckSession($db);
 if ($s->IsAdmin()) {
     if (!is_numeric($_GET['id'])) JSON::outError('wrong input', 400);
-    $r = $db->q('DELETE FROM comments WHERE idComments = ?', array($_GET['id']))->fetch();
+    $r = $db->q('DELETE FROM Comments WHERE idComments = ?', array($_GET['id']))->fetch();
     if (!$r) JSON::outError ('Такого комментария не существует', 500);
     echo json_encode(array('result'=>'success'));
 } else {
