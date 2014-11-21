39fe5cbabc1374dc01cdedb4006f80abbe0f61e8
Victor Gorokhov <me@rkit.pp.ru>
2014-11-22 02:17:29 +0300

    error_handler check

--- Comments/JSON.php
+++ Comments/JSON.php
@@ -1,5 +1,20 @@
 <?php
 namespace Comments;
+/**
+ * @param $exception \Exception
+ */
+function JSON_exception_handler($exception) {
+    JSON::outError($exception->getMessage());
+}
+function jsonErrorHandler($errno, $errstr, $errfile, $errline)
+{
+    switch ($errno){
+        case E_NOTICE:
+            break;
+        default:
+            JSON::outError("[$errfile:$errline] $errstr");
+    }
+}
 class JSON {
     /**
      * @param string $msg
@@ -22,25 +37,8 @@ class JSON {
         header("Access-Control-Allow-Headers: Content-Type");
         header("Content-type: application/json");
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') die();
-
-        function myErrorHandler($errno, $errstr, $errfile, $errline)
-        {
-            switch ($errno){
-                case E_NOTICE:
-                    break;
-                default:
-                    JSON::outError("[$errfile:$errline] $errstr");
-            }
-        }
-        set_error_handler("myErrorHandler");
-
-        /**
-         * @param $exception \Exception
-         */
-        function exception_handler($exception) {
-            JSON::outError($exception->getMessage());
-        }
-        set_exception_handler('exception_handler');
+        set_error_handler("jsonErrorHandler");
+        set_exception_handler('JSON_exception_handler');
     }
 
     /**
